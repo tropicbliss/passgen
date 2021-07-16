a435796bd81d35d48e33c3ec1c55b3727a43fd94
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2021-07-16 10:43:07 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,22 @@ fn main() {
             std::process::exit(0);
         }
     };
+    
+    // Get generated password
     let password = options.generate_password();
+    
+    // Output generated password
     cli::print_generated_password(&password);
+    
+    // Copy to clipboard
     cli::copy_to_clipboard(&password).expect("error copying to clipboard");
+    
     cli::clipboard_success_prompt();
     if options.save {
+        
+        // Save to file
         cli::save_password(&password).expect("error saving password");
+        
         cli::save_success_prompt();
     }
 }

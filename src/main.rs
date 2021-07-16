140bbf356d986ca5bbc7677952f0df5b67e0ba08
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2021-07-16 17:17:54 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,12 @@ fn main() {
     cli.print_generated_password();
 
     // Copy to clipboard
-    cli.copy_to_clipboard().expect("error copying to clipboard");
+    cli.copy_to_clipboard();
 
     cli::clipboard_success_prompt();
     if options.save {
         // Save to file
-        cli.save_password();
+        cli.save_password().expect("error saving password to file");
 
         cli::save_success_prompt();
     }
